c6942d06e3a806fdf11b12998717f28b968b7b75
Chris Oates <chrispoates@gmail.com>
2021-06-21 14:29:20 -0400

    Renewing work on project.

--- app.js
+++ app.js
@@ -42,45 +42,154 @@ const words = [
   {
     value: "wizard",
     type: "noun",
-    surreal_adj: ["glistening"],
+    verbs: ["tossed","cooked","saw","made friends with","went caroling with","waddled over to"]
+  },
+  {
+    value: "shaman",
+    type: "noun",
+    verbs: ["tossed","cooked","saw","made friends with","went caroling with","waddled over to"]
+  },
+  {
+    value: "programmer",
+    type: "noun",
+    verbs: ["tossed","cooked","saw","made friends with","went caroling with","harmonized with"]
   },
   {
     value: "cheeseburger",
     type: "noun",
-    surreal_adj: ["glistening", "itchy"],
-    surreal_verb: ["cooked", "saw"],
+    verbs: []
   },
   {
-    value: "pencil",
-    type: "noun",
-    surreal_adj: ["itchy"],
-    surreal_verb: ["cooked", "saw"],
-  },
-  {
-    value: "glistening",
+    value: "tasty",
     type: "adjective",
+    nouns: ["cheeseburger","apple"]
   },
   {
-    value: "nice",
+    value: "cheerful",
     type: "adjective",
+    nouns: ["wizard","programmer","friend","knave","cowboy","doctor","clerk","doctor","shaman"]
   },
   {
-    value: "itchy",
+    value: "salt-of-the-earth",
     type: "adjective",
-  },
-  {
-    value: "hot",
-    type: "adjective",
+    nouns: ["wizard","programmer","friend","knave","cowboy","doctor","clerk","doctor","shaman"]
   },
   {
     value: "cooked",
     type: "verb",
-    surreal_obj: ["pencil"],
+    objs: ["cheeseburger","apple"],
   },
   {
     value: "saw",
     type: "verb",
+    objs: ["cheeseburger","wizard","programmer","friend","duck","knave"],
   },
+//   
+  {
+    value: "tossed",
+    type: "verb",
+    objs: ["cheeseburger","friend","apple"],
+  },
+  {
+    value: "made friends with",
+    type: "verb",
+    objs: ["wizard","programmer","friend","duck","knave","cowboy","doctor"],
+  },
+  {
+    value: "friend",
+    type: "noun",
+    verbs: ["tossed","cooked","saw"],
+  },
+  {
+    value: "apple",
+    type: "noun",
+  },
+  {
+    value: "clerk",
+    type: "noun",
+    verbs: ["tossed","cooked","saw","made friends with","went caroling with","was jealous of","harmonized with"],
+  },
+  {
+    value: "duck",
+    type: "noun",
+    verbs: ["saw","waddled over to"]
+  },
+  {
+    value: "no-nonsense",
+    type: "adjective",
+    nouns: ["wizard","programmer","friend","duck","knave","cowboy","doctor","clerk","doctor","shaman"],
+  },
+  {
+    value: "nimble",
+    type: "adjective",
+    nouns: ["wizard","programmer","friend","duck","knave","cowboy","doctor","clerk","doctor","shaman"],
+  },
+  {
+    value: "doctor",
+    type: "noun",
+    verbs: ["tossed","cooked","saw","made friends with","went caroling with","was jealous of","harmonized with"],
+  },
+  {
+    value: "cowboy",
+    type: "noun",
+    verbs: ["tossed","cooked","saw","made friends with","went caroling with","was jealous of","harmonized with"],
+  },
+  {
+    value: "went caroling with",
+    type: "verb",
+    objs: ["wizard","programmer","friend","knave","doctor","clerk","shaman","doctor"],
+  },
+  {
+    value: "was jealous of",
+    type: "verb",
+    objs: ["wizard","programmer","friend","knave","doctor","clerk","shaman","doctor"],
+  },
+  {
+    value: "waddled over to",
+    type: "verb",
+    objs: ["cheeseburger","wizard","programmer","friend","knave","doctor","clerk","shaman","doctor"],
+  },
+  {
+    value: "iridescent",
+    type: "adjective",
+    objs: [],
+  },
+  {
+    value: "disintegrated",
+    type: "verb",
+    objs: [],
+  },
+  {
+    value: "harmonized with",
+    type: "verb",
+    objs: ["wizard","programmer","friend","knave","doctor","clerk","shaman","doctor"],
+  },
+  {
+    value: "broke",
+    type: "adjective",
+    nouns: ["wizard","programmer","friend","knave","doctor","clerk","shaman","doctor","cowboy"],
+  },
+  {
+    value: "flirty",
+    type: "adjective",
+    nouns: ["wizard","programmer","friend","knave","doctor","clerk","shaman","doctor","cowboy"],
+  },
+  {
+    value: "capable",
+    type: "adjective",
+    nouns: ["wizard","programmer","friend","knave","doctor","clerk","shaman","doctor","cowboy"],
+  },
+  {
+    value: "greasy",
+    type: "adjective",
+    nouns: ["cheeseburger"],
+  },
+  {
+    value: "gallant",
+    type: "adjective",
+    nouns: ["wizard","programmer","friend","knave","doctor","clerk","shaman","doctor","cowboy"],
+  },
+  
 ];
 
 app.get("/", function (req, res) {
@@ -89,109 +198,106 @@ app.get("/", function (req, res) {
   });
 });
 
+app.get("/surreal", function (req, res) {
+    res.render("index", {
+      sentence: surrealSentence(words),
+    });
+  });
+
 app.listen("3000", function () {
   console.log("Server listening on port 3000.");
 });
 
-function randNounPhrase(nounArr, adjArr) {
-  console.log("length is " + adjArr[getRandomInt(0, adjArr.length)]);
-  return (
-    adjArr[getRandomInt(0, adjArr.length)] +
-    " " +
-    nounArr[getRandomInt(0, nounArr.length)]
-  );
-}
-
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-}
-
 function surrealSentence(words) {
-  let nouns = words.filter((x) => {
-    return x.type === "noun";
-  });
-  let subj = nouns[getRandomInt(0, nouns.length)].value;
-  //subj = "cheeseburger";
-  let adj = getSurrealAdj(subj, words);
-  let verb = getSurrealVerb(subj, words);
+    let nouns = words.filter((x) => x.type === "noun");
+    let subj = nouns[Math.floor(Math.random() * nouns.length)].value; 
+    let verb = generateSurrealVerb(subj,words);
+    let adj = generateSurrealAdjective(subj,words);
+    let obj = nouns[Math.floor(Math.random() * nouns.length)].value;
 
-  // generate noun
-  // let adj = find surreal adjective function
-  // let verb = find surreal verb
-  // let object = find surreal object
-
-  return `The ${adj} ${subj} ${verb}`;
-}
-
-// Finds a "surreal" adjective for the given noun. If no such adjective exists, it chooose a normal one. If no adjectives exist, it returns "".
-function getSurrealAdj(noun, words) {
-  let surrealAdjs = words.find((x) => {
-    return noun === x.value;
-  })?.surreal_adj;
-
-  if (surrealAdjs === undefined) {
-    let adjs = words
-      .filter((x) => {
-        return x.type === "adjective";
-      })
-      .map((x) => x.value);
-    return adjs[getRandomInt(0, adjs.length)];
-  } else {
-    return surrealAdjs[getRandomInt(0, surrealAdjs.length)] ?? "";
-  }
-}
-
-function getSurrealVerb(noun, words) {
-  // alternate way where surreal verb is stored in noun
-  let surrealVerbs = words.find((wrd) => wrd.value === noun).surreal_verb;
-  if (surrealVerbs) {
-    return surrealVerbs[getRandomInt(0, surrealVerbs.length)];
-  } else {
-    let verbs = words.filter((x) => x.type === "verb");
-    return verbs[getRandomInt(0, verbs.length)].value;
-  }
+    return `The ${adj} ${subj} ${verb} ${addAn(obj)}.`;
 }
 
 function normalSentence(words) {
-  let nouns = words.filter((x) => {
-    return x.type === "noun";
-  });
-  let subj = nouns[getRandomInt(0, nouns.length)].value;
-  let adj = getNormalAdj(subj, words);
-  let verb = getNormalVerb(subj, words);
-  let obj = getNormalObject(verb, words);
-  return `The ${adj} ${subj} ${verb} a ${obj}`;
+    let subj = generateSubject(words);
+    if (typeof subj === 'undefined') {
+        return "Data invalid :(";
+    }
+    
+    let adj = getAdj(subj, words);
+    let verb = getVerb(subj, words);
+    let obj = getObj(verb, words);
+    
+    return `The ${adj} ${subj} ${verb} ${addAn(obj)}.`;
 }
 
-// if a normal adjective for that noun exists, it returns a random one. Otherwise, it returns "".
-function getNormalAdj(noun, words) {
-  let nounObj = words.find((x) => x.value === noun);
-  let adjs = words.filter((x) => {
-    let isAdj = x.type === "adjective";
-    let isSurreal = nounObj?.surreal_adj?.includes(x.value);
-    return isAdj && !isSurreal;
-  });
-  return adjs[Math.floor(Math.random() * adjs.length)].value ?? "";
+// creates the subject of the sentence but returns undefined if there are no nouns or if no nouns can be subjects of verbs
+function generateSubject(words) {
+    let nouns = words.filter((x) => x.type === "noun" && x.verbs?.length > 0);
+    if (nouns.length === 0) {
+        return undefined;
+    } else {
+        return nouns[Math.floor(Math.random() * nouns.length)].value;
+    }
 }
 
-function getNormalVerb(noun, words) {
-  let nounObj = words.find((x) => x.value === noun);
-  let verbs = words.filter((x) => {
-    let isVerb = x.type === "verb";
-    let isSurreal = nounObj?.surreal_verb?.includes(x.value);
-    return isVerb && !isSurreal;
-  });
-  return verbs[Math.floor(Math.random() * verbs.length)]?.value ?? "";
+// returns a normal sounding adjective for that noun, or "" if it can't find one.
+function getAdj(noun, words) {
+    let adjs = words.filter((x) => x.type === "adjective" && x.nouns?.includes(noun));
+    if (adjs.length === 0) {
+        return "";
+    } else {
+        return adjs[Math.floor(Math.random() * adjs.length)].value;
+    }
 }
 
-function getNormalObject(verb, words) {
-  let verbData = words.find((x) => x.value === verb);
-  let nouns = words.filter((x) => {
-    let isNoun = x.type === "noun";
-    let isSurreal = verbData?.surreal_obj?.includes(x.value);
-    return isNoun && !isSurreal;
-  });
-  return nouns[Math.floor(Math.random() * nouns.length)].value ?? "";
+// returns a normal sounding verb for that noun, or "" if it can't find one.
+function getVerb(noun, words) {
+    const verbs = words.find((x) => x.value === noun)?.verbs;
+    if (typeof verbs === 'undefined') {
+        return "";
+    } else {
+        return verbs[Math.floor(Math.random() * verbs.length)];
+    }
+}
+
+function getObj(verb, words) {
+    let verbObj = words.find(x => x.value === verb);
+    let objs = verbObj?.objs;
+    if (typeof objs === 'undefined') {
+        return "";
+    } else {
+        return objs[Math.floor(Math.random() * objs.length)];
+    }
+}
+
+function addAn (nounPhrase) {
+    let vowels = ['a','e','i','o','u'];
+    let firstLetter = nounPhrase.charAt(0);
+
+    if (vowels.includes(firstLetter)) {
+        return "an " + nounPhrase;
+    } else {
+        return "a " + nounPhrase;
+    }
+}
+
+// randomly choose an adjective that is NOT supposed go with that noun (i.e. doesn't contain that noun in its list of acceptable nouns).
+function generateSurrealAdjective(noun,words) {
+    let adjs = words.filter((x) => x.type === "adjective" && !x.nouns?.includes(noun)); // notice the ! 
+    if (adjs.length === 0) {
+        return "";
+    } else {
+        return adjs[Math.floor(Math.random() * adjs.length)].value;
+    }
+}
+
+function generateSurrealVerb(noun,words) {
+    let nounData = words.find((x) => x.value === noun);
+    let verbs = words.filter((x) => x.type === "verb" && !nounData.verbs?.includes(x.value)); // notice the ! 
+    if (verbs.length === 0) {
+        return "";
+    } else {
+        return verbs[Math.floor(Math.random() * verbs.length)].value;
+    }
 }
